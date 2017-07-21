7f52a954d23c624b64ecde9ad728f56b9a9385f5
Victor Ngo <psychosete@gmail.com>
2017-07-21 15:25:01 -0700

    (test) Change around server code to debug heroku problem

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = require('./app');
 const db = require('../db');
 const PORT = process.env.port || 3000;
 
+
 app.listen(PORT, () => {
+  console.log('Env -->', JSON.stringify(process.env));
   console.log('NSA listening on port 3000!');
 });
