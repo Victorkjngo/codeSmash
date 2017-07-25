e21dfbe74603bf33acc1533808848c15f5571a19
Jun Park <jun.park@live.com>
2017-07-25 11:54:41 -0700

    React Router/Mongo db/Save Code in Playground (#10)
    * Refactor hashHistory for ReactRouter to browserHistory
    
    * Add proper switching for Playground.jsx component. Create routes for new Pricing and Dashboard components
    
    * Add styling changes to navBar
    
    * Remove border-radius on navbar
    
    * Remove extra div that caused Playground.jsx not to render to the full width/height of the screen
    
    * Add database schema for MongoDB
    
    * Add Mongoose as a dependency in the package.json
    
    * Implement API route for GET and POST to Users table in Mongo
    
    * Error handling for POSTs to the DB
    
    * Add Axios as dependency. Create saveCodeSnippet function in View.jsx
    
    * Initial saveCodeSnippet and fetch functions created for POST and GET to DB
    
    * Initial routes for POST and GET to the DB

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const Users = ('../index.js');
+const app = express();
 
 router.route('/')
   .get((req, res) => {
@@ -10,5 +12,19 @@ router.route('/')
     console.log('in the correct route');
     res.status(201).send({ data: 'Posted!' });
   });
+
+app.get('/api/users', (req, res) => {
+  res.status(200).send(Users.selectAll());
+});
+
+app.post('/api/users', (req, res) => {
+// Temporary to refresh the database with every POST
+  Users.collection.drop();
+  console.log('POSTing to Users', req.body);
+  Users.create(req.body, (err) => {
+    if (err) { res.status(300).send('ERROR POSTING USER', err); } 
+  });
+  res.status(201).send({ data: 'POST to Users' });
+});
 
 module.exports = router;
